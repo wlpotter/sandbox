3e2e07a36f6f3b0669b40966613c0a21472f23d7
wpotter20 <44096602+wpotter20@users.noreply.github.com>
2021-02-13 15:08:55 -0600

    Script successful

--- LRDA-csv-parsing/csv-parsing.py
+++ LRDA-csv-parsing/csv-parsing.py
@@ -1,5 +1,6 @@
 import csv
-
+# NOTE: Requires a data source to be identified. To maintain confidentiality,
+# I have not included the data inputs and outputs in the public git repo
 
 def transform_date_name_string(string):
     """ (str) -> [str, str]
@@ -40,8 +41,29 @@ def add_spaces(no_space_string):
         i += 1
     return space_string
 
+
+def create_short_description(category, folder_name):
+    transformed_date_name_string = transform_date_name_string(folder_name)
+    name = transformed_date_name_string[1]
+    date = transformed_date_name_string[0]
+    location_preamble = ""
+    if (category == "Location"):
+        location_preamble = name + ", "
+    return "Name: " + name + "\n" + "Date: " + date + "\nLocation: "\
+        + location_preamble + "Saint Louis, MO, USA\nDescription: \nExtra: "
+
+
 data_source = "lrda_data_source.csv"
+output_file = open("lrda_data_output.csv", "w+")
 with open(data_source, newline='', encoding='utf-8') as f:
     parser = csv.reader(f, delimiter="\t")
+    writer = csv.writer(output_file, delimiter=' ', quotechar='"', quoting=csv.QUOTE_ALL)
+    i = 0
     for row in parser:
-        print(transform_date_name_string(row[1]))
+        short_description = create_short_description(row[0], row[1])
+        if i != 0:
+            row[5] = short_description
+        writer.writerow(row)
+        # write the row
+        i += 1
+        print(short_description + "\n---\n")
